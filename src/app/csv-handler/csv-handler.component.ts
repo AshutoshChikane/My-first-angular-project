040e985d492562f603a7434ed6e34b16252a4ddf
Ashutosh Sudhir Chikane <ashutoshc@AD.NSEIT.COM>
2025-01-31 21:56:30 +0530

    31-01-25 v2

--- src/app/csv-handler/csv-handler.component.ts
+++ src/app/csv-handler/csv-handler.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CsvHandleServiceService } from '../my-service/csv-handler/csv-handle-service.service'
 
 @Component({
   selector: 'app-csv-handler',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './csv-handler.component.css'
 })
 export class CsvHandlerComponent {
-  
+  @Input() data!: any[];
+
+  constructor(private mycsv_handler:CsvHandleServiceService){}
+  downloadCsvClick(): void{
+    console.log("helo", this.data)
+    this.mycsv_handler.downloadCsv(this.data, "my_first_export.csv")
+  }
 }
--- src/app/my-service/csv-handler/csv-handle-service.service.ts
+++ src/app/my-service/csv-handler/csv-handle-service.service.ts
@@ -6,4 +6,26 @@ import { Injectable } from '@angular/core';
 export class CsvHandleServiceService {
 
   constructor() { }
+
+  downloadCsv(data: any[],filename:string = 'export.csv'): void{
+    const csvData = this.convertToCSV(data);
+    
+    // Create a Blob with CSV data and trigger a download
+    const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
+    const link = document.createElement('a');
+    if (link.download !== undefined) { // Feature detection for download attribute
+      const url = URL.createObjectURL(blob);
+      link.setAttribute('href', url);
+      link.setAttribute('download', filename);
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  }
+
+  private convertToCSV(data: any[]): string {
+    const headers = Object.keys(data[0]);
+    const rows = data.map(row => headers.map(header => row[header]).join(','));
+    return [headers.join(','), ...rows].join('\n');
+  }
 }
